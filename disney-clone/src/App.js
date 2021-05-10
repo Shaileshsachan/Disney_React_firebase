99bcefbc0d7e92678f1464cc26049c5900ac54f9
Shaileshsachan <46265361+Shaileshsachan@users.noreply.github.com>
2021-05-10 12:22:25 +0530

    'header.js'

---
+++ disney-clone/src/App.js
@@ -0,0 +1,21 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header";
+import Login from "./components/Login";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <Login />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
